fca0f0a5ac2d4fa4cd6d4de1b1694117ba1921ea
aemeadow <60167778+aemeadow@users.noreply.github.com>
2021-04-06 11:53:07 -0400

    timer. finish line works for players, not yet for npc.

--- G390_JagerMeadows_Drag/Assets/finishline.cs
+++ G390_JagerMeadows_Drag/Assets/finishline.cs
@@ -1,23 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class finishline : MonoBehaviour
 {
 
     public GameObject playerwin;
     public GameObject npcwin;
+    public Text timeText;
+    public float startTime = 0f;
+
+    bool lapComplete = false;
+    
+    void FixedUpdate()
+    {
+        if(lapComplete == false)
+        {
+            startTime += Time.deltaTime;
+            timeText.text = "Time: " + startTime;
+    }
+        }
+        
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             playerwin.SetActive(true);
+            lapComplete = true;
         }
 
-        if (other.gameObject.tag == "opponent")
+        elseif (startTime == 100f)
         {
             npcwin.SetActive(true);
+            lapComplete = true;
         }
     }
 }
