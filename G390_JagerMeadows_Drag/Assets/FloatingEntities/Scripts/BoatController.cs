33f8f05d35603283b2d6f7f5d01a5219359cd77e
njager <nicholasejager@gmail.com>
2021-04-06 10:32:13 -0400

    changed inputs for boat

--- G390_JagerMeadows_Drag/Assets/FloatingEntities/Scripts/BoatController.cs
+++ G390_JagerMeadows_Drag/Assets/FloatingEntities/Scripts/BoatController.cs
@@ -9,16 +9,21 @@ public class BoatController : MonoBehaviour
   void Update()
   {
 
-    if (Input.GetKey(KeyCode.Q))
+    /*if (Input.GetKey(KeyCode.Q))
       ship.RudderLeft();
     if (Input.GetKey(KeyCode.D))
+      ship.RudderRight();*/
+
+    if (Input.GetAxis("Horizontal") < 0)
+      ship.RudderLeft();
+    if (Input.GetAxis("Horizontal") > 0)
       ship.RudderRight();
 
     if (forward)
     {
-      if (Input.GetKey(KeyCode.Z))
+      if (Input.GetKey(KeyCode.JoystickButton5))
         ship.ThrottleUp();
-      else if (Input.GetKey(KeyCode.S))
+      else if (Input.GetKey(KeyCode.JoystickButton4))
       {
         ship.ThrottleDown();
         ship.Brake();
@@ -26,24 +31,24 @@ public class BoatController : MonoBehaviour
     }
     else
     {
-      if (Input.GetKey(KeyCode.S))
+      if (Input.GetKey(KeyCode.JoystickButton4))
         ship.ThrottleUp();
-      else if (Input.GetKey(KeyCode.Z))
+      else if (Input.GetKey(KeyCode.JoystickButton5))
       {
         ship.ThrottleDown();
         ship.Brake();
       }
     }
 
-    if (!Input.GetKey(KeyCode.Z) && !Input.GetKey(KeyCode.S))
+    if (!Input.GetKey(KeyCode.JoystickButton5) && !Input.GetKey(KeyCode.JoystickButton4))
       ship.ThrottleDown();
 
-    if (ship.engine_rpm == 0 && Input.GetKeyDown(KeyCode.S) && forward)
+    if (ship.engine_rpm == 0 && Input.GetKey(KeyCode.JoystickButton4) && forward)
     {
       forward = false;
       ship.Reverse();
     }
-    else if (ship.engine_rpm == 0 && Input.GetKeyDown(KeyCode.Z) && !forward)
+    else if (ship.engine_rpm == 0 && Input.GetKey(KeyCode.JoystickButton5) && !forward)
     {
       forward = true;
       ship.Reverse();
